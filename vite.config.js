21956638640940405604296a79efb2502fcb7449
taqin98 <taqinjunior56@gmail.com>
2026-02-24 15:26:43 +0700

    [added] workflows github actions

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  return {
-    plugins: [react()],
-    base: env.VITE_BASE_PATH || '/',
-  }
+export default defineConfig({
+  plugins: [react()],
+  // Jika di GitHub Actions (production), gunakan /ikatancinta/
+  base: process.env.NODE_ENV === 'production' ? '/ikatancinta/' : '/',
 })
